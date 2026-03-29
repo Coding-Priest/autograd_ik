b09c151d1581f6b6eb3c49d4b531ae5c4642f976
AlphaGotReal <subhampatra2411@gmail.com>
2026-03-29 22:47:49 +0530

    mujoco template

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <mujoco/mujoco.h>
+
+int main(int argc, char **argv) {
+  printf("Successfully linked against MuJoCo version: %s\n", mj_versionString());
+  return 0;
+}
